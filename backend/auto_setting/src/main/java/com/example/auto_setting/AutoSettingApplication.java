4114374aa246bf7716ca0aeaef161b0813472473
bokyung1500 <bokyung1500@naver.com>
2024-09-29 16:54:00 +0900

    기본 내용 commit

---
+++ backend/auto_setting/src/main/java/com/example/auto_setting/AutoSettingApplication.java
@@ -0,0 +1,13 @@
+package com.example.auto_setting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSettingApplication.class, args);
+	}
+
+}
