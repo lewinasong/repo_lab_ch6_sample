ef54c399199e49983dc654d846034dd4eac99956
unknown <bokyung1500@naver.com>
2024-10-18 17:46:11 +0900

    PBK 20241018

---
+++ backend/auto_setting/src/main/java/com/example/auto_setting/service/ProgramDto.java
@@ -0,0 +1,51 @@
+package com.example.auto_setting.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProgramDto {
+    private String pgmId;
+    private String pgmNm;
+    private String empNo;
+    private String filePath;
+    private LocalDateTime sysRegDtm;
+    private LocalDateTime sysUpdDtm;
+
+    // Getters and Setters
+    /*
+    public String getPgmId() { return pgmId; }
+
+    public void setPgmId(String pgmId) { this.pgmId = pgmId; }
+
+    public String getPgmNm() {
+        return pgmNm;
+    }
+
+    public void setPgmNm(String pgmNm) {
+        this.pgmNm = pgmNm;
+    }
+
+    public String getEmpNo() {
+        return empNo;
+    }
+
+    public void setEmpNo(String empNo) {
+        this.empNo = empNo;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {this.filePath = filePath; }
+
+     */
+}
