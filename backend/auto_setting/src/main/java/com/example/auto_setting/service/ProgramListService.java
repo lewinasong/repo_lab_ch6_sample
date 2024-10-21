9dba677d492cdd8497c7bded454e18e10b80a89f
ye-jin-choi128 <choiyj128@naver.com>
2024-10-22 01:38:26 +0900

    Update ProgramListService.java
    etProgramListWithNameByEmpNo 메서드 추가:
    이 메서드는 ProgramListJdbcRepository에서 findProgramListWithNameByEmpNo 메서드를 호출하여, 직원 번호(empNo)를 기준으로 프로그램 리스트와 프로그램 이름을 조합하여 반환합니다.

--- backend/auto_setting/src/main/java/com/example/auto_setting/service/ProgramListService.java
+++ backend/auto_setting/src/main/java/com/example/auto_setting/service/ProgramListService.java
@@ -1,7 +1,7 @@
 package com.example.auto_setting.service;
 
 import com.example.auto_setting.entity.ProgramList;
-import com.example.auto_setting.persistence.ProgramListRepository;
+import com.example.auto_setting.persistence.ProgramListJdbcRepository;
 import com.example.auto_setting.util.ProgramListConverter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -9,16 +9,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class ProgramListService {
 
-    private final ProgramListRepository programListRepository;
+    private final ProgramListJdbcRepository programListRepository;
 
+    // 직원 번호로 Program List를 조회하여 프로그램 ID 리스트 반환
     public List<Integer> getProgramIdListByEmpNo(@NonNull final String empNo) {
-
         // DB에서 EMP_NO를 통해 ProgramList를 조회
         final ProgramList programList = programListRepository.findByEmpNo(empNo);
 
@@ -27,6 +28,12 @@ public class ProgramListService {
         return programIdlist;
     }
 
+    // 직원 번호로 Program List와 Program Name을 조합하여 반환하는 서비스 메서드
+    public List<Map<String, Object>> getProgramListWithNameByEmpNo(@NonNull String empNo) {
+        return programListRepository.findProgramListWithNameByEmpNo(empNo);
+    }
+
+    // 직원 번호로 Program List를 생성하는 메서드
     public void createProgramList(@NonNull final String empNo, @NonNull final List<Integer> programList) {
         // List<Integer>를 쉼표로 구분된 String으로 변환
         final String convertedProgramIdList = ProgramListConverter.convertListToString(programList);
@@ -34,6 +41,7 @@ public class ProgramListService {
         programListRepository.create(empNo, convertedProgramIdList);
     }
 
+    // 직원 번호로 Program List를 업데이트하는 메서드
     public void updateProgramList(@NonNull final String empNo, @NonNull final List<Integer> programList) {
         // List<Integer>를 쉼표로 구분된 String으로 변환
         final String convertedProgramIdList = ProgramListConverter.convertListToString(programList);
@@ -41,6 +49,7 @@ public class ProgramListService {
         programListRepository.update(empNo, convertedProgramIdList);
     }
 
+    // 직원 번호로 Program List를 삭제하는 메서드
     public void deleteProgramList(@NonNull final String empNo) {
         programListRepository.deleteByEmpNo(empNo);
     }
